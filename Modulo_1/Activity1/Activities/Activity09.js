5bc3d8dce8491124f1bfd351c7fa8f9415fc042c
JefersonBLuz <jefersonbrluz@gamil.com>
2025-03-10 02:29:22 -0300

    Separação da Atividade 8

--- Modulo_1/Activity1/Activities/Activity09.js
+++
@@ -1,8 +0,0 @@
-const input = require('prompt-sync')()
-const Question_9 = ()=>{
-    console.log(`9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console utilizando um loop for.\n`)
-    for(let i = 10; i>0;i--){
-        console.log(i);
-    }
-}
-module.exports = Question_9
